e57fb2cffca14d03905550c27749044957b24d36
Afonso Machado <afonsosmachado@gmail.com>
2024-02-28 17:37:32 -0300

    create field class

--- lib/models/field.dart
+++ lib/models/field.dart
@@ -1,3 +1,5 @@
+import 'package:minefield/models/explosion_exception.dart';
+
 class Field {
   final int row;
   final int column;
@@ -24,5 +26,68 @@ class Field {
 
   void open() {
     if (_isOpened) return;
+
+    _isOpened = true;
+
+    if (_hasMine) {
+      _isExploded = true;
+      throw ExplosionException();
+    }
+
+    if (safeNeighborhood) {
+      for (var neighbor in neighbors) {
+        neighbor.open();
+      }
+    }
+  }
+
+  void revealMine() {
+    if (_hasMine) _isOpened = true;
+  }
+
+  void putMine() {
+    _hasMine = true;
+  }
+
+  void toggleMark() {
+    _isMarked = !_isMarked;
+  }
+
+  void restart() {
+    _isOpened = false;
+    _isMarked = false;
+    _hasMine = false;
+    _isExploded = false;
+  }
+
+  bool get solved {
+    bool hasMineAndIsMarked = _hasMine && _isMarked;
+    bool safeAndIsOpened = !_hasMine && _isOpened;
+    return hasMineAndIsMarked || safeAndIsOpened;
+  }
+
+  // Nenhum dos vizinhos tem mina
+  bool get safeNeighborhood {
+    return neighbors.every((neighbor) => !neighbor._hasMine);
+  }
+
+  int get minesQuantityInTheNeighborhood {
+    return neighbors.where((neighbor) => neighbor._hasMine).length;
+  }
+
+  bool get hasMine {
+    return _hasMine;
+  }
+
+  bool get isExploded {
+    return _isExploded;
+  }
+
+  bool get isOpened {
+    return _isOpened;
+  }
+
+  bool get isMarked {
+    return _isMarked;
   }
 }
