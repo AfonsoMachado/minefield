c1a4d6a5f6b3a2e039bcc2028d40a01e4ff1107e
Afonso Machado <afonsosmachado@gmail.com>
2024-02-29 15:50:55 -0300

    convert minefield app to stateful

--- lib/screens/minefield_app.dart
+++ lib/screens/minefield_app.dart
@@ -4,8 +4,16 @@ import 'package:minefield/components/result_widget.dart';
 import 'package:minefield/models/board.dart';
 import 'package:minefield/models/field.dart';
 
-class MinefieldApp extends StatelessWidget {
+class MinefieldApp extends StatefulWidget {
   const MinefieldApp({super.key});
+
+  @override
+  State<MinefieldApp> createState() => _MinefieldAppState();
+}
+
+class _MinefieldAppState extends State<MinefieldApp> {
+  bool? _won;
+  final Board _board = Board(rows: 12, columns: 12, minesQuantity: 3);
 
   void _restart() {
     print('Reiniciar...');
@@ -25,11 +33,11 @@ class MinefieldApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: ResultWidget(
-          won: null,
+          won: _won,
           onRestart: _restart,
         ),
         body: BoardWidget(
-          board: Board(rows: 15, columns: 15, minesQuantity: 0),
+          board: _board,
           onOpen: _open,
           onToggleMark: _onToggleMark,
         ),
